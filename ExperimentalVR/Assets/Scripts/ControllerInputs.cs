26d07acc81aa71174ab0633d3f6ba76c99e27a7c
RR <rruppel@uos.de>
2019-09-26 19:15:23 +0200

    postion brush in prefab! Controller Input done - reset works => scaling must be changed - start animation is missing

--- ExperimentalVR/Assets/Scripts/ControllerInputs.cs
+++ ExperimentalVR/Assets/Scripts/ControllerInputs.cs
@@ -4,11 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+
 using Valve.VR;
-
-// https://docs.unity3d.com/Manual/OpenVRControllers.html 
-
-// https://medium.com/@sarthakghosh/a-complete-guide-to-the-steamvr-2-0-input-system-in-unity-380e3b1b3311
 
 [ExecuteInEditMode]
 public class ControllerInputs : MonoBehaviour
@@ -26,13 +23,16 @@ public class ControllerInputs : MonoBehaviour
     #endregion
 
 
+    [SerializeField] private ParticipantController participant;
+    [SerializeField] private KeyCode resetKey;
+
     // a reference to the action
     public SteamVR_Action_Boolean itsHammerTime;
+    public SteamVR_Action_Boolean itsRestTime;
     // a reference to the hand
     public SteamVR_Input_Sources InputSource;
 
 
-    [SerializeField] private ParticipantController participant;
 
     // Use this for initialization
     void Start()
@@ -40,18 +40,47 @@ public class ControllerInputs : MonoBehaviour
         if (itsHammerTime != null)
         {
             itsHammerTime.AddOnStateDownListener(TriggerDown, InputSource);
+
+        }
+
+        if (itsRestTime != null)
+        {
+            itsRestTime.AddOnStateDownListener(GrapDown, InputSource);
+
         }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(resetKey))
+        {
+            ResetPosition();
+        }
+    }
+
 
     void OnDisable()
     {
         if (itsHammerTime != null)
         {
             itsHammerTime.RemoveOnStateDownListener(TriggerDown, InputSource);
-            
+
+        }
+
+        if (itsRestTime != null)
+        {
+            itsRestTime.RemoveOnStateDownListener(GrapDown, InputSource);
+
         }
     }
-    
+
+    public void GrapDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Grap is down");
+        // start Animation
+    }
+
 
     public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
